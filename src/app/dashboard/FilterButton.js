f87f588e106fc1562d1d0b60838bd557254d093e
Wendell Costa <wendellbarreto2007@gmail.com>
2025-03-11 12:28:57 -0300

    adding responsiveness, as well as candlestick charts to OHLC visualization

--- src/app/dashboard/FilterButton.js
+++ src/app/dashboard/FilterButton.js
@@ -71,44 +71,6 @@ export default function FilterButton({ initialData }) {
                     Reset
                 </button>
             </div>
-
-            <div className="overflow-x-auto">
-                <table className="w-full p-4 flex flex-col justify-start min-w-full border border-gray-300">
-                    <thead className="flex w-full">
-                        <tr className="w-full flex justify-start items-center font-semibold text-gray-500">
-                            <th className="px-4 py-2">Name</th>
-                            <th className="px-4 py-2">Price</th>
-                            <th className="px-4 py-2">Change</th>
-                            <th className="px-4 py-2">High</th>
-                            <th className="px-4 py-2">Low</th>
-                            <th className="px-4 py-2">Market Cap</th>
-                            <th className="px-4 py-2">Market Rank</th>
-                        </tr>
-                    </thead>
-                    <tbody className="flex flex-col w-full m-3">
-                        {cryptocurrencies.length === 0 ? (
-                            <tr>
-                                <td colSpan="7" className="text-center">No data available</td>
-                            </tr>
-                        ) : (
-                            (filteredCryptos.length > 0 ? filteredCryptos : cryptocurrencies).map((crypto) => (
-                                <Cell
-                                    key={crypto.id}
-                                    id={crypto.id}
-                                    name={crypto.name}
-                                    image={crypto.image}
-                                    current_price={crypto.current_price}
-                                    change={crypto.price_change_24h}
-                                    high={crypto.high_24h}
-                                    low={crypto.low_24h}
-                                    market_cap={crypto.market_cap}
-                                    market_cap_rank={crypto.market_cap_rank}
-                                />
-                            ))
-                        )}
-                    </tbody>
-                </table>
-            </div>
         </>
     );
 }
